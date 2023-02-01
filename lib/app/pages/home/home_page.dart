c24d3b8f385d3c60232836db39eb1df43a4d4bac
Gustavo Almeida <gualmeida2004@gmail.com>
2023-02-01 16:13:53 -0300

    Mixins

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,15 +1,25 @@
+import 'package:delivery_app/app/core/ui/helpers/loader.dart';
+import 'package:delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:delivery_app/app/core/ui/widgets/delivery_app_bar.dart';
 import 'package:delivery_app/app/models/product_model.dart';
 import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: DeliveryAppBar(),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          showError('Error');
+        }),
         body: Column(
           children: [
             Expanded(
