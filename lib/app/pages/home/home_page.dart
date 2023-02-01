6dbd8eaf03cdcd5673867b84ccf9bf38dc24fd3d
Gustavo Almeida <gualmeida2004@gmail.com>
2023-02-01 17:16:13 -0300

    Implementing base state

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -6,6 +6,7 @@ import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../core/ui/base_state/base_state.dart';
 import 'home_state.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,13 +16,10 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with Loader, Messages {
+class _HomePageState extends BaseState<HomePage, HomeController> {
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      context.read<HomeController>().loadProducts();
-    });
+  void onReady() {
+    controller.loadProducts();
   }
 
   @override
